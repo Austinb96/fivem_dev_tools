d86174c1db2a7d27bb93c546311937d4fc87864a
Austinb96 <austin.m.babcock@gmaIl.com>
2025-03-03 13:33:31 -0600

    codewalkercli: add ymap conversion

--- src-codewalker/CodeWalkerLib/Program.cs
+++ src-codewalker/CodeWalkerLib/Program.cs
@@ -54,108 +54,12 @@ public class Program
         }
 
         Console.WriteLine("[INFO] Initializing cache");
-        var dlcPaths = String.Join(";", new[]
-    {
-        // "mpBeach",
-        // "mpBusiness",
-        // "mpChristmas",
-        // "mpValentines",
-        // "mpBusiness2",
-        // "mpHipster",
-        // "mpIndependence",
-        // "mpPilot",
-        // "spUpgrade",
-        // "mpLTS",
-        // "mpheist",
-        // "mppatchesng",
-        // "patchday1ng",
-        // "patchday2ng",
-        // "mpchristmas2",
-        // "patchday2bng",
-        "patchday3ng"
-        // "patchday4ng",
-        // "mpluxe",
-        // "patchday5ng",
-        // "mpluxe2",
-        // "patchday6ng",
-        // "mpreplay",
-        // "patchday7ng",
-        // "mplowrider",
-        // "mphalloween",
-        // "patchday8ng",
-        // "mpapartment",
-        // "mpxmas_604490",
-        // "mplowrider2",
-        // "mpjanuary2016",
-        // "mpvalentines2",
-        // "patchday9ng",
-        // "mpexecutive",
-        // "patchday10ng",
-        // "mpstunt",
-        // "patchday11ng",
-        // "mpimportexport",
-        // "mpbiker",
-        // "patchday12ng",
-        // "patchday13ng",
-        // "mpspecialraces",
-        // "mpgunrunning",
-        // "mpairraces",
-        // "mpsmuggler",
-        // "mpchristmas2017",
-        // "mpassault",
-        // "mpbattle",
-        // "patchday14ng",
-        // "patchday15ng",
-        // "patchday16ng",
-        // "patchday17ng",
-        // "patchday18ng",
-        // "patchday19ng",
-        // "patchday20ng",
-        // "mpchristmas2018",
-        // "patchday21ng",
-        // "mpvinewood",
-        // "patchday22ng",
-        // "mpheist3",
-        // "mpsum",
-        // "patchday23ng",
-        // "mpheist4",
-        // "patchday24ng",
-        // "mptuner",
-        // "patchday25ng",
-        // "mpsecurity",
-        // "patchday26ng",
-        // "mpg9ec",
-        // "patchdayg9ecng",
-        // "mpsum2",
-        // "patchday27ng",
-        // "mpsum2_g9ec",
-        // "patchday27g9ecng",
-        // "mpchristmas3",
-        // "mpchristmas3_g9ec",
-        // "patchday28ng",
-        // "patchday28g9ecng",
-        // "mp2023_01",
-        // "patch2023_01",
-        // "patch2023_01_g9ec",
-        // "mp2023_01_g9ec",
-        // "mp2023_02",
-        // "patch2023_02",
-        // "mp2023_02_g9ec",
-        // "mp2024_01",
-        // "patch2024_01",
-        // "patch2024_01_g9ec",
-        // "mp2024_01_g9ec",
-        // "mp2024_02",
-        // "mp2024_02_g9ec",
-        // "patch2024_02"
-    });
-
         GTA5Keys.LoadFromPath(GTA_PATH, null);
         gameFileCache = new GameFileCache(
             2147483648,
             10,
             GTA_PATH,
-            dlcPaths,
+            "",
             true,
             "Installers;_CommonRedist"
         );
@@ -196,8 +100,28 @@ public class Program
                     XmlDocument xml = new XmlDocument();
                     xml.Load(inputPath);
                     var psoFile = XmlPso.GetPso(xml);
-                    var ymtData = psoFile.Save();
-                    File.WriteAllBytes(outputPath, ymtData);
+                    var psoData = psoFile.Save();
+                    File.WriteAllBytes(outputPath, psoData);
+                    break;
+                    
+                case "ymt2xml":
+                    InitCodeWalkerCache();
+                    Console.WriteLine("[INFO] Converting YMT to XML");
+                    var ymt = new YmtFile();
+                    var ymtData = File.ReadAllBytes(inputPath);
+                    ymt.Load(ymtData);
+                    var ymtXmlContent = MetaXml.GetXml(ymt, out var ymtXml);
+                    File.WriteAllText(outputPath, ymtXmlContent);
+                    break;
+                    
+                case "ymap2xml":
+                    InitCodeWalkerCache();
+                    Console.WriteLine("[INFO] Converting YMAP to XML");
+                    var ymap = new YmapFile();
+                    var ymapData = File.ReadAllBytes(inputPath);
+                    ymap.Load(ymapData);
+                    var ymapXmlContent = MetaXml.GetXml(ymap, out var ymapXml);
+                    File.WriteAllText(outputPath, ymapXmlContent);
                     break;
 
                 default:
